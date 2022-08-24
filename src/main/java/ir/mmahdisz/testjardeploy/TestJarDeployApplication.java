5e4a3de131176b3dc529b8b918fde22d1da21ed9
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2022-08-24 16:32:28 +0430

    Initial commit

---
+++ src/main/java/ir/mmahdisz/testjardeploy/TestJarDeployApplication.java
@@ -0,0 +1,13 @@
+package ir.mmahdisz.testjardeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJarDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJarDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mmahdisz/testjardeploy/TestJarDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mmahdisz.testjardeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJarDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
